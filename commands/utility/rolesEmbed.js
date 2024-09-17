bc6550d1998e8fa37e138feec13ac8a058bca768
John Fries <w.bababooie@gmail.com>
2024-09-17 23:59:36 +0100

    Update rolesEmbed.js

--- commands/utility/rolesEmbed.js
+++ commands/utility/rolesEmbed.js
@@ -1,37 +1,64 @@
-const { SlashCommandBulder, EmbedBuilder, ActionRowBuilder } = require('discord.js');
+const { eventTeamRole, roleChannel, reactionRoleMessageId } = require('../../config.json');
+const { PermissionFlagsBits, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, EmbedBuilder, ButtonStyle } = require('discord.js');
 const { blue } = require('../../colors.json');
-const { eventTeamRole, roleChannel } = require('../../config.json');
+const fs = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('rolesEmbed')
-        .setDescription('Create a basic reaction role button embed send to the roles channel.'),
+        .setName('embed')
+        .setDescription('Create a basic reaction role button embed send to the roles channel.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
-        const role = guild.roles.cache.get(eventTeamRole);
-        const channel = guild.channels.cache.get(roleChannel);
+        const role = interaction.guild.roles.cache.get(eventTeamRole);
+        const channel = interaction.guild.channels.cache.get(roleChannel);
 
-        const embed = new EmbedBuilder()
-            .setTitle('Adriens Event Team Role')
-            .setDescription('If You Get This Role You Will Be Added To The Adriens Event Team')
-            .setColor(blue)
-        const message = await channel.send({ embeds: [embed] });
-
-        const row = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId('event_team_role')
-                .setLabel('Adriens Team Role')
-                .setStyle('Primary')
-        );
-
-        message.edit({ components: [new ActionRowBuilder().addComponents(row)] });
-        const filter = i => i.customId === 'event_team_role';
-        const collector = message.createMessageComponentCollector({ filter, time: 15000 });
-        collector.on('collect', async i => {
-            i.deferUpdate();
-            const member = guild.members.cache.get(i.user.id);
-            member.roles.add(eventTeamRole);
+        let message;
+        if (reactionRoleMessageId) {
+            try {
+                message = await channel.messages.fetch(reactionRoleMessageId);
+            } catch (error) {
+                console.error("Could not fetch the reaction role message. It may have been deleted or the ID is incorrect.");
+            }
         }
-        )
+        if (!message) {
+            const embed = new EmbedBuilder()
+                .setTitle('Reaction Roles')
+                .setDescription('Click on the designated button to get the role.')
+                .setColor(blue);
+
+            message = await channel.send({ embeds: [embed] });
+
+            const config = require('../../config.json');
+            config.reactionRoleMessageId = message.id;
+            fs.writeFileSync('../../config.json', JSON.stringify(config, null, 2));
+        }
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('eventteam')
+                    .setLabel('Adriens Team')
+                    .setStyle(ButtonStyle.Primary)
+            );
+        await message.edit({ components: [row] }).catch(error => {
+            console.error("Failed to edit the message. It may have been deleted or modified:", error);
+        });
+
+        const filter = i => i.customId === 'eventteam';
+        const collector = message.createMessageComponentCollector({ filter, time: 0 }); // No timeout for persistence
+
+        collector.on('collect', async i => {
+            await i.deferUpdate();
+            const member = interaction.guild.members.cache.get(i.user.id);
+
+            if (member) {
+                if (member.roles.cache.has(eventTeamRole)) {
+                    await member.roles.remove(eventTeamRole);
+                    await i.user.send(`You have been **removed** from the role: ${role.name}`);
+                } else {
+                    await member.roles.add(eventTeamRole);
+                    await i.user.send(`You have been **given** the role: ${role.name}`);
+                }
+            }
+        });
     },
 };
